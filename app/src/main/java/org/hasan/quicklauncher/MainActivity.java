45bd705b721874d3a23638a869b8cf51d8befa7e
Mahmudul <mhasan0047@gmail.com>
2018-05-28 19:58:00 -0400

    button2,webpage

--- app/src/main/java/org/hasan/quicklauncher/MainActivity.java
+++ app/src/main/java/org/hasan/quicklauncher/MainActivity.java
@@ -4,6 +4,7 @@ Clicking Google: Opens up google app*/
 package org.hasan.quicklauncher;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,14 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //LAUNCHING AN ACTIVITY WITHIN APP (SHARING INFO BETWEEN ACTIVITIES):
         Button secActivityBtn = (Button) findViewById(R.id.secondActivityBtn); //just referenced the button in layout
         secActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //a) LINKING THE ACTIVITY AND THE INTENT:
-                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class); //Intent(get_the_applications_Context, its_Class).
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class); //getting this application's information
                     //^Creating an Intent object called "startIntent". Already made a class/activity already called "SecondActivity"
-                //b) PASSING INFORMATION TO ANOTHER ACTIVITY:
+                //b) PASSING INFORMATION TO ANOTHER ACTIVITY/CLASS:
                 startIntent.putExtra("org.hasan.quicklauncher.hello", "Hello World!");
                     //^Can put a lot of extra information in a bundle and send it to the other activity. the other activity can unbundle it and use the info.
                     //Key-Value Pairs: "packageName.nameOfKey" , "value you want to pass"
@@ -31,8 +33,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(startIntent);
                     /*The onClick listener of the "Second Activity" Button is going to say
                     "Hey i have this INTENT, the user is interacting with this button. What do we want to do with it?"
-                    The INTENT is to go to the SecondActivity, so go start that.
+                    The INTENT is to go to the SecondActivity, so go start that....Switch to the SecondActivity class
                      */
+            }
+        });
+
+        //LAUNCHING ACTIVITY OUTSIDE APP:
+        Button googleActivityBtn = (Button) findViewById(R.id.googleBtn); //just referenced the button in layout
+        googleActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "https://www.google.com/"; //making a string that holds the url
+                Uri webaddress = Uri.parse(google); //parse (divide into small components to be analyzed) the url as a URI (universal resource identifier)
+
+                //Intent is to launch something on the android device that can browse to that webpage
+                Intent goToGoogle = new Intent(Intent.ACTION_VIEW, webaddress)  //going outside of application to do an action (launch). sending it to the webaddress to try and open up
+                //Will send out a broadcast asking "can any apps on this device that can do this (the action that's being requested, opening to this webaddress)?"
+                //Need to see if the response back is NULL(meaning "no there are no apps that can do this):
+                if (goToGoogle.resolveActivity(getPackageManager()) != NULL)
             }
         });
 
--- app/src/main/java/org/hasan/quicklauncher/SecondActivity.java
+++ app/src/main/java/org/hasan/quicklauncher/SecondActivity.java
@@ -10,19 +10,20 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-            //When the app runs, it first runs the Activity files (look at the AndroidManifest.xml file)
-            //So it can run the SecondActivity or the MainActivity java file first. SecondActivity can get information or it may not.
-            //Will make it so that SecondActivity DOES get information (so if it runs first, it just switches to  MainActivity?)
+
+        //When the app runs, it first runs the Activity files (look at the AndroidManifest.xml file)
+        //So it can run the SecondActivity or the MainActivity java file first. SecondActivity can get information or it may not.
+        //Will make it so that SecondActivity DOES get information (so if it runs first, it just switches to  MainActivity?)
 
         //LAUNCHING AN ACTIVITY WITHIN APP (SHARING INFO BETWEEN ACTIVITIES):
         if (getIntent().hasExtra("org.hasan.quicklauncher.hello")) {  //if the intent has extra information, such as (the key), then do the following:
                 //1) Making sure MainActivity runs so this SecondAvtivity can get information (unpackage info)
                 //I already put the value of  "Hello World!" in the "org.hasan.quicklauncher.hello" package/key
-            TextView tview = (TextView) findViewById(R.id.textView);
-                //2)^ "tv" will hold a reference to the TextView display in the 2nd layout
             String text = getIntent().getExtras().getString("org.hasan.quicklauncher.hello");
-                //3)^ *unloading package: [ String text = getIntent().getExtras().getString("sent package's Key") ]
+                //2)^ *unloading package: [ String text = getIntent().getExtras().getString("sent package's Key") ]
                 //"text" will hold a reference to whatever the value of the key "org.hasan.quicklauncher.hello" is.
+            TextView tview = (TextView) findViewById(R.id.textView);
+                //3)^ "tv" will hold a reference to the TextView display in the 2nd layout
             tview.setText(text);
                 //4)^ will put that text in the TextView box
         }
