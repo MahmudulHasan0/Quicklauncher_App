c35b5de5acfb775e9370bae29aef29d905c7bd4f
Mahmudul <mhasan0047@gmail.com>
2018-05-29 19:36:14 -0400

    finished.

--- app/src/main/java/org/hasan/quicklauncher/MainActivity.java
+++ app/src/main/java/org/hasan/quicklauncher/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //LAUNCHING AN ACTIVITY WITHIN APP (SHARING INFO BETWEEN ACTIVITIES):
+    //***LAUNCHING AN ACTIVITY WITHIN APP (SHARING INFO BETWEEN ACTIVITIES):***
         Button secActivityBtn = (Button) findViewById(R.id.secondActivityBtn); //just referenced the button in layout
         secActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,24 +33,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(startIntent);
                     /*The onClick listener of the "Second Activity" Button is going to say
                     "Hey i have this INTENT, the user is interacting with this button. What do we want to do with it?"
-                    The INTENT is to go to the SecondActivity, so go start that....Switch to the SecondActivity class
-                     */
+                    The INTENT is to go to the SecondActivity, so go start that....Switch to the SecondActivity class*/
             }
         });
 
-        //LAUNCHING ACTIVITY OUTSIDE APP:
+    //***LAUNCHING ACTIVITY OUTSIDE APP:***
         Button googleActivityBtn = (Button) findViewById(R.id.googleBtn); //just referenced the button in layout
         googleActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String google = "https://www.google.com/"; //making a string that holds the url
                 Uri webaddress = Uri.parse(google); //parse (divide into small components to be analyzed) the url as a URI (universal resource identifier)
-
                 //Intent is to launch something on the android device that can browse to that webpage
-                Intent goToGoogle = new Intent(Intent.ACTION_VIEW, webaddress)  //going outside of application to do an action (launch). sending it to the webaddress to try and open up
-                //Will send out a broadcast asking "can any apps on this device that can do this (the action that's being requested, opening to this webaddress)?"
-                //Need to see if the response back is NULL(meaning "no there are no apps that can do this):
-                if (goToGoogle.resolveActivity(getPackageManager()) != NULL)
+                Intent goToGoogle = new Intent(Intent.ACTION_VIEW, webaddress);  //going outside of application to do an action (launch). sending it to the webaddress to try and open up
+                    //Will send out a broadcast asking "can any apps on this device that can do this (the action that's being requested, opening to this webaddress)?"
+                    //Need to see if the response back is NULL(meaning "no there are no apps that can do this):
+                if (goToGoogle.resolveActivity(getPackageManager()) != null) { //is the response back is "yes theres an app that can do this".
+                    //^If there is a list of something, that can do this, then go ahead and give me that activity
+                    startActivity(goToGoogle);
+                }
             }
         });
 
